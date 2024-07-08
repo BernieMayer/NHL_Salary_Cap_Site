07d4780322847725c7224bb4387dd77f999dd80f
Bernie Mayer <berniemayer94@gmail.com>
2024-07-08 13:37:49 -0600

    Add Team model

--- NHL_SalaryCapSite/db/schema.rb
+++ NHL_SalaryCapSite/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_182848) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_193643) do
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.string "team"
     t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
