0a88aa9cd5aa2135390c1003b470658cc66d3953
Bernie Mayer <berniemayer94@gmail.com>
2024-07-13 12:51:15 -0600

    Add basic Salary Cap hit total rake task and spec

--- NHL_SalaryCapSite/lib/tasks/populate_salary_cap_totals.rake
+++ NHL_SalaryCapSite/lib/tasks/populate_salary_cap_totals.rake
@@ -2,7 +2,22 @@ namespace :salary_cap do
     desc 'Populate salary cap totals'
     task populate: :environment do
 
-        
+        Team.all.each do |team|
+            puts "Team is #{team.name}"
+            (2024...2030).each do |year|
+                salary_cap_total = SalaryCapTotal.find_by(team: team, year: year)
+
+                next if salary_cap_total.nil?
+
+                cap_hits_total = 0.0
+                team.players.each do |player|
+                    puts "Adding cap hit"
+                    cap_hits_total += CapHit.find_by(team: team, year:year, player: player).cap_value
+                end
+                salary_cap_total.total = cap_hits_total
+                salary_cap_total.save!
+            end
+        end
   
       puts 'Salary cap totals populated successfully.'
     end
--- NHL_SalaryCapSite/spec/tasks/populate_salary_cap_totals_spec.rb
+++ NHL_SalaryCapSite/spec/tasks/populate_salary_cap_totals_spec.rb
@@ -8,12 +8,18 @@ describe 'salary_cap:populate' do
       end
 
     context "One cap hit" do
-        let!(:team) { Team.create(name: "Calgary Flames")}
-        let!(:player) { Player.create(name: "Dustin Wolf")}
+        let!(:team) { Team.create(name: "Calgary Flames", code: "CGY")}
+        let!(:player) { Player.create(name: "Dustin Wolf", team: team, position: "G")}
         let!(:player_cap_hit) {  9000000.0}
-        let!(:cap_hit) {CapHit.create(team_id: team.id, player: player, year: 2024,cap_value:player_cap_hit )}
+        let!(:cap_hit) {CapHit.create(team_id: team.id, player_id: player.id, year: 2024, cap_value:player_cap_hit )}
+        let!(:salary_cap_total) {SalaryCapTotal.create(team: team, year: 2024, total: 0.0)}
 
         it "the total should equal the cap hit" do
+            
+            Rake::Task['salary_cap:populate'].invoke
+            
+            expect(Player.all.count).to eq(1)
+            expect(team.players.length).to eq(1)
             expect(SalaryCapTotal.find_by(team: team).total).to eq(player_cap_hit)
         end
     end
