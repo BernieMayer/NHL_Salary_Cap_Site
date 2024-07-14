8f5762181bba9f55522ad4fbfba9440481014a1d
Bernie Mayer <berniemayer94@gmail.com>
2024-07-14 11:48:22 -0600

    Refactor specs and fix an issue causing the specs to fail

--- NHL_SalaryCapSite/spec/tasks/populate_salary_cap_totals_spec.rb
+++ NHL_SalaryCapSite/spec/tasks/populate_salary_cap_totals_spec.rb
@@ -2,19 +2,29 @@ require 'rails_helper'
 require 'rake'
 
 describe 'salary_cap:populate' do
-    after(:context) do
-        # Clean up the database before each test
-        # Team.destroy_all
-        # Player.destroy_all
-        # CapHit.destroy_all
-        # SalaryCapTotal.destroy_all
-
-    end
-
-    before(:all) do
-    
+    before(:each) do
         Rake.application.load_rakefile
         Rake::Task['salary_cap:populate'].reenable
+    end
+
+    context "One cap hit" do
+        let!(:team) { Team.create(name: "Calgary Flames", code: "CGY")}
+        let!(:player) { Player.create(name: "Dustin Wolf", team: team, position: "G")}
+        let!(:player_cap_hit) { 9000000.0 }
+        let!(:cap_hit) {CapHit.create(team_id: team.id, player_id: player.id, year: 2024, cap_value:player_cap_hit )}
+        let!(:salary_cap_total) {SalaryCapTotal.create(team: team, year: 2024, total: 0.0)}
+
+        it "the total should equal the cap hit" do
+            
+            expect do
+                Rake::Task['salary_cap:populate'].invoke
+                salary_cap_total.reload
+            end.to change { salary_cap_total.total}.from(0.0).to(player_cap_hit)
+            
+            expect(SalaryCapTotal.all.count).to eq(1)
+            expect(Player.all.count).to eq(1)
+            expect(team.players.length).to eq(1)
+        end
     end
 
     context "Multiple teams" do
@@ -33,36 +43,17 @@ describe 'salary_cap:populate' do
         
         it "the total should equal the cap hit" do
             
-            Rake::Task['salary_cap:populate'].invoke
+            expect do
+                Rake::Task['salary_cap:populate'].invoke
+                salary_cap_total.reload
+                salary_cap_total_2.reload
+            end.to change{ salary_cap_total.total }.from(0.0).to(player_cap_hit)
+            .and change{ salary_cap_total_2.total }.from(0.0).to(player_cap_hit_2)
 
             expect(Team.all.count).to eq(2)
             expect(Player.all.count).to eq(2)
             expect(CapHit.all.count).to eq(2)
             expect(SalaryCapTotal.all.count).to eq(2)
-
-
-            expect(SalaryCapTotal.find_by(team: team_1).total).to eq(player_cap_hit)
-            expect(SalaryCapTotal.find_by(team: team_2).total).to eq(player_cap_hit_2)
-        end
-
-
-    end
-
-    context "One cap hit" do
-        let!(:team) { Team.create(name: "Calgary Flames", code: "CGY")}
-        let!(:player) { Player.create(name: "Dustin Wolf", team: team, position: "G")}
-        let!(:player_cap_hit) { 9000000.0 }
-        let!(:cap_hit) {CapHit.create(team_id: team.id, player_id: player.id, year: 2024, cap_value:player_cap_hit )}
-        let!(:salary_cap_total) {SalaryCapTotal.create(team: team, year: 2024, total: 0.0)}
-
-        it "the total should equal the cap hit" do
-            
-            Rake::Task['salary_cap:populate'].invoke
-            
-            expect(SalaryCapTotal.all.count).to eq(1)
-            expect(Player.all.count).to eq(1)
-            expect(team.players.length).to eq(1)
-            expect(SalaryCapTotal.find_by(team: team).total).to eq(player_cap_hit)
         end
     end
 
@@ -72,7 +63,7 @@ describe 'salary_cap:populate' do
         let!(:player_2) { Player.create(name: "Blake Coleman", team: team, position: "C") }
         let!(:player_3) { Player.create(name: "Kevin Bahl", team: team, position: "D") }
         let!(:player_cap_hit) {  9000000.0 }
-        let!(:blake_coleman_cap_hit) {43000000.0 }
+        let!(:blake_coleman_cap_hit) { 43000000.0 }
         let!(:kevin_bahl_cap_hit) {10000000.0 }
         let!(:cap_hit) { CapHit.create(team_id: team.id, player_id: player.id, year: 2024, cap_value: player_cap_hit )}
         let!(:cap_hit_1) { CapHit.create(team_id: team.id, player_id: player_2.id, year: 2024, cap_value: blake_coleman_cap_hit)}
@@ -81,15 +72,13 @@ describe 'salary_cap:populate' do
         let!(:expected_cap_hit) {62000000.0}
 
         it "the total should equal the cap hit sum" do
-            
             Rake::Task['salary_cap:populate'].invoke
             
-
+            expect(SalaryCapTotal.all.count).to eq(1)
             expect(Player.all.count).to eq(3)
             expect(team.players.length).to eq(3)
             expect(CapHit.all.count).to eq(3)
            
-            
             expect(SalaryCapTotal.find_by(team: team).total).to eq(expected_cap_hit)
         end
 
@@ -100,7 +89,6 @@ describe 'salary_cap:populate' do
             let!(:salary_cap_total_2) {SalaryCapTotal.create(team: team, year: 2025, total: 0.0)}
 
             it "should sum the total for all years of data" do 
-
                 Rake::Task['salary_cap:populate'].invoke
 
                 expect(SalaryCapTotal.all.count).to eq(2)
