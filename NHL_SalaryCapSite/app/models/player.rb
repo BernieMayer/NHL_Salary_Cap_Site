befde2929ba7811447a6668a25b27ac6398a6372
Bernie Mayer <berniemayer94@gmail.com>
2024-07-15 13:43:49 -0600

    Fix scopes to get all positions correctly

--- NHL_SalaryCapSite/app/models/player.rb
+++ NHL_SalaryCapSite/app/models/player.rb
@@ -6,7 +6,7 @@ class Player < ApplicationRecord
     validates :team, presence: true
     validates :position, presence: true
 
-    scope :forwards, -> { where(position: ["C", "LW", "RW"]) }
-    scope :defence, -> { where(position: ["D", "LD", "RD"]) }
+    scope :forwards, -> { where("LOWER(position) LIKE '%c%' OR LOWER(position) LIKE '%lw%' OR LOWER(position) LIKE '%rw%'") }
+    scope :defence, -> {where("LOWER(position) LIKE '%d%' OR LOWER(position) LIKE '%ld%' OR LOWER(position) LIKE '%rd%'") }
     scope :goalies, -> { where(position: ["G"])}
 end
--- NHL_SalaryCapSite/spec/modals/player_spec.rb
+++ NHL_SalaryCapSite/spec/modals/player_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe Player, type: :model do
         let!(:forward1) { Player.create(name: "Blake Coleman", position: 'C', team: team) }
         let!(:forward2) { Player.create(name: "Ryan Lomberg", position: 'LW', team: team) }
         let!(:forward3) { Player.create(name: "Matt Coronato", position: 'RW', team: team) }
+        let!(:forward4) { Player.create(name: "Jonathan Huberdeau", position: "LW, RW", team: team)}
         let!(:defenseman) { Player.create(name: "Jake Bean", position: 'D', team: team) }
         let!(:goalie) { Player.create(name: "Dustin Wolf", position: 'G', team: team) }
 
         it 'returns only the forwards' do
-            expect(Player.forwards).to match_array([forward1, forward2, forward3])
+            expect(Player.forwards).to match_array([forward1, forward2, forward3, forward4])
         end
     end
 
@@ -23,10 +24,11 @@ RSpec.describe Player, type: :model do
         let!(:defenseman) { Player.create(name: "Jake Bean", position: 'D', team: team) }
         let!(:defenseman1) { Player.create(name: "Kevin Bahl", position: "LD", team: team)}
         let!(:defenseman2) { Player.create(name: "Hunter Brzustewicz", position: "RD", team: team)}
+        let(:defenseman3) { Player.create(name: "John Doe", position:"LD, RD, D", team: team)}
         let!(:goalie) { Player.create(name: "Dustin Wolf", position: 'G', team: team) }
 
         it 'returns only the defence' do
-            expect(Player.defence).to match_array([defenseman, defenseman1, defenseman2])
+            expect(Player.defence).to match_array([defenseman, defenseman1, defenseman2, defenseman3])
         end
     end
 
