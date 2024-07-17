9589009c7894b08e36084c8c83ebccc63ef14e46
Bernie Mayer <berniemayer94@gmail.com>
2024-07-16 22:16:28 -0600

    Team should display only roster players

--- NHL_SalaryCapSite/app/controllers/teams_controller.rb
+++ NHL_SalaryCapSite/app/controllers/teams_controller.rb
@@ -5,7 +5,7 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find_by!(code: params[:code]) 
-    @players = @team.players.includes(:cap_hits)
+    @players = @team.players.roster.includes(:cap_hits)
     @salary_cap_total_2024 = @team.salary_cap_totals.find_by(year:2024).total
     
 
