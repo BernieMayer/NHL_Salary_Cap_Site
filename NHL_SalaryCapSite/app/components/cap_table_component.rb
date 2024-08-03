fa6205dfbbf6e84444ddaccca7c84da812c52499
Bernie Mayer <berniemayer94@gmail.com>
2024-08-03 12:46:44 -0600

    Display buyouts properly

--- NHL_SalaryCapSite/app/components/cap_table_component.rb
+++ NHL_SalaryCapSite/app/components/cap_table_component.rb
@@ -3,7 +3,16 @@
 class CapTableComponent < ViewComponent::Base
     attr_reader :players
 
-    def initialize(players:)
+    def initialize(players:, cap_type: 'Roster')
         @players = players
+        @cap_type = cap_type
+    end
+
+    def get_cap_hit(player, year)
+        if @cap_type == "Roster" 
+            player.team_cap_hits.find_by(year: year)&.cap_value
+        elsif @cap_type == "Buyout"
+            player.cap_hits.buyout.find_by(year: year)&.cap_value
+        end
     end
 end
