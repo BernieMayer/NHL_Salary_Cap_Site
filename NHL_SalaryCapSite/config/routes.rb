76a8932940dafccdfa4ab0a326da3bd244dbf2b4
Bernie Mayer <berniemayer94@gmail.com>
2024-08-06 13:59:32 -0600

    Remove auth

--- NHL_SalaryCapSite/config/routes.rb
+++ NHL_SalaryCapSite/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  authenticate :admin_user do
     mount Motor::Admin => '/admin'
-  end
   get 'home/index'
   get 'players/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
