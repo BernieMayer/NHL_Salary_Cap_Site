39737019aad5b7b3a516c279fdfd6fb3f266c202
Bernie Mayer <berniemayer94@gmail.com>
2024-08-09 22:07:23 -0600

    Add current cap space calculation

--- NHL_SalaryCapSite/app/models/salary_cap_total.rb
+++ NHL_SalaryCapSite/app/models/salary_cap_total.rb
@@ -1,6 +1,16 @@
 class SalaryCapTotal < ApplicationRecord
   belongs_to :team
 
+  SALARY_CAP_2024 = 88000000
+
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :year, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  scope :year, ->(year) { where(year: year) }
+
+  def calculate_cap_space
+    if self.year == 2024 
+      SALARY_CAP_2024 - self.total
+    end
+  end
 end
