f5d458f8881818a5abafbf5845ae900a9112a38e
Bernie Mayer <berniemayer94@gmail.com>
2024-08-11 10:44:02 -0600

    Add start to free agent component

---
+++ NHL_SalaryCapSite/app/components/free_agent_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class FreeAgentComponent < ViewComponent::Base
+
+end
---
+++ NHL_SalaryCapSite/test/components/free_agent_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class FreeAgentComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(FreeAgentComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
