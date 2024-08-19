27ad45f77d9ef8537d301d065500c36730f92d2f
Bernie Mayer <berniemayer94@gmail.com>
2024-08-19 13:06:03 -0600

    Create contract table

--- NHL_SalaryCapSite/db/schema.rb
+++ NHL_SalaryCapSite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_08_041030) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_19_190536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_041030) do
     t.text "cap_type", default: "Roster"
     t.index ["player_id"], name: "idx_16946_index_cap_hits_on_player_id"
     t.index ["team_id"], name: "idx_16946_index_cap_hits_on_team_id"
+  end
+
+  create_table "contracts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "motor_alert_locks", force: :cascade do |t|
