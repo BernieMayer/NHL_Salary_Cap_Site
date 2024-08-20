1427fc99cdb6bea53c339fe80584869820406dc0
Bernie Mayer <berniemayer94@gmail.com>
2024-08-20 17:22:46 -0600

    Add debugging

--- NHL_SalaryCapSite/app/controllers/api/imports_controller.rb
+++ NHL_SalaryCapSite/app/controllers/api/imports_controller.rb
@@ -31,21 +31,28 @@ module Api
     private
 
     def authorize_request
-      puts request.inspect
+      Rails.logger.info "Request details: #{request.inspect}"
       auth_header = request.headers['Authorization']
       if auth_header.present?
         token = auth_header.split(' ').last
-        puts "the auth_header is #{auth_header}"
-        puts "the token is #{token}"
+        Rails.logger.info "Authorization header: #{auth_header}"
+        Rails.logger.info "Token: #{token}"
+    
         begin
           decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })
-        rescue JWT::DecodeError
+          Rails.logger.info "Decoded Token: #{decoded_token}"
+          # Handle decoded_token as needed
+        rescue JWT::DecodeError => e
+          Rails.logger.error "JWT Decode Error: #{e.message}"
           render json: { error: 'Unauthorized' }, status: :unauthorized
+        rescue StandardError => e
+          Rails.logger.error "General Error: #{e.message}"
+          render json: { error: 'Internal server error' }, status: :internal_server_error
         end
       else
         render json: { error: 'Authorization header missing' }, status: :unauthorized
       end
-    end
+    end    
   end
 end
 
