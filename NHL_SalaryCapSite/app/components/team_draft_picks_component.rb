017722831fec7311ddfd9fd5502e77d6c1a8e617
Bernie Mayer <berniemayer94@gmail.com>
2024-08-24 11:33:36 -0600

    Add basic component and fix specs

--- NHL_SalaryCapSite/app/components/team_draft_picks_component.rb
+++ NHL_SalaryCapSite/app/components/team_draft_picks_component.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class TeamDraftPicksComponent < ViewComponent::Base
+  def initialize(team:)
+    @team = team
+  end
 
+  def draft_picks
+    @team.draft_picks
+  end
 end
--- NHL_SalaryCapSite/spec/components/team_draft_picks_component_spec.rb
+++ NHL_SalaryCapSite/spec/components/team_draft_picks_component_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TeamDraftPicksComponent, type: :component do
   let!(:team) { Team.create(name:"Calgary Flames", code: "CGY") }
-  
-  it "renders the draft picks table" do
-    render_inline(TeamDraftPicksComponent.new(team: team))
 
-    expect(rendered_component).to have_text("Round 1")
-    expect(rendered_component).to have_text("Round 4")
-    expect(rendered_component).to have_text("2025")
+  it "renders the draft picks table" do
+    rendered_component = render_inline(TeamDraftPicksComponent.new(team: team))
+
+    expect(rendered_component.text).to include("Round 1")
+    expect(rendered_component.text).to include("Round 4")
+    expect(rendered_component.text).to include("2025")
 
   end
 end
