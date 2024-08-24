27a9df4bd353c9ad1fa39efec2d1c0647f3473dc
Bernie Mayer <berniemayer94@gmail.com>
2024-08-24 14:42:21 -0600

    Display the count of draft picks

--- NHL_SalaryCapSite/app/components/team_draft_picks_component.rb
+++ NHL_SalaryCapSite/app/components/team_draft_picks_component.rb
@@ -3,6 +3,7 @@
 class TeamDraftPicksComponent < ViewComponent::Base
   def initialize(team:)
     @team = team
+    @draft_picks_by_year_and_round = @team.draft_picks_by_year_and_round
   end
 
   def draft_picks
