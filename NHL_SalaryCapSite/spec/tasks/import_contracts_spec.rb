987f43cd23d801ff5a2f49f88fd899ec31f4bf01
Bernie Mayer <berniemayer94@gmail.com>
2024-09-27 19:45:57 -0600

    Add unit tests for boston and anaheim

--- NHL_SalaryCapSite/spec/tasks/import_contracts_spec.rb
+++ NHL_SalaryCapSite/spec/tasks/import_contracts_spec.rb
@@ -3,58 +3,91 @@ require 'rake'
 
 RSpec.describe 'import:contracts', type: :task do
   let(:task_name) { 'import:contracts' }
-  let(:json_file_path) { Rails.root.join('spec', 'fixtures', 'files', 'calgary_flames.json') }
+  let!(:json_file_path) { fixture_file_upload Rails.root.join('spec', 'fixtures', 'files', 'anaheim_ducks.json') }
 
   before do
     # Explicitly load the rake task file
     Rake.application.rake_require('lib/tasks/import_contracts', [Rails.root.to_s])
     Rake::Task.define_task(:environment)
+    Rake::Task['import:contracts'].reenable
   end
 
-  it 'imports contract data from a JSON file' do
-    # Stub file path and existence check to return true
-    allow(File).to receive(:exist?).with(json_file_path.to_s).and_return(true)
+  context "boston bruins" do
+    let!(:json_file_path) { fixture_file_upload Rails.root.join('spec', 'fixtures', 'files', 'boston_bruins.json') }
 
-    # Stub file reading
-    json_data = File.read(json_file_path)
-    allow(File).to receive(:read).with(json_file_path.to_s).and_return(json_data)
+    it 'imports contract data from a JSON file' do
+      # Create a team for testing
+      team = Team.create!(name: "Boston Bruins", code: "BOS", gm: nil, coach: nil)
 
-    # Stub JSON parsing
-    parsed_json = JSON.parse(json_data)
-    allow(JSON).to receive(:parse).with(json_data).and_return(parsed_json)
+      # Run the rake task and capture output
+      Rake::Task[task_name].invoke(json_file_path) 
 
-    # Create a team for testing
-    team = Team.create!(name: "Calgary Flames", code: "CGY", gm: nil, coach: nil)
+      # Reload team data to check for updates
+      team.reload
+      expect(team.gm).to eq('Don Sweeney')
+      expect(team.coach).to eq('Jim Montgomery')
 
-    # Run the rake task and capture output
-    expect { Rake::Task[task_name].invoke(json_file_path.to_s) }.to output(/Data import complete!/).to_stdout
+      expect(DraftPick.all.length).to eq(21)
+      expect(Player.all.length).to eq(46)
 
-    # Reload team data to check for updates
-    team.reload
-    expect(team.gm).to eq('Brad Treliving')
-    expect(team.coach).to eq('Darryl Sutter')
+      # Check players have been imported
+      player = Player.find_by(name: 'Michael DiPietro')
+      expect(player).not_to be_nil
+      # expect(player.position).to eq('RW, LW')
+      # expect(player.career_games_played).to eq(0)
+      expect(player.contracts.length).to eq(2)
 
-    # Check players have been imported
-    player = Player.find_by(name: 'Johnny Gaudreau')
-    expect(player).not_to be_nil
-    expect(player.position).to eq('LW')
-    expect(player.career_games_played).to eq(500)
 
-    defense_player = Player.find_by(name: 'Rasmus Andersson')
-    expect(defense_player).not_to be_nil
-    expect(defense_player.position).to eq('D')
 
-    goalie = Player.find_by(name: 'Jacob Markstrom')
-    expect(goalie).not_to be_nil
-    expect(goalie.position).to eq('G')
+    end
 
-    # Check contracts have been created
-    contract = Contract.find_by(player_id: player.id)
-    expect(contract).not_to be_nil
-    expect(contract.expiry_status).to eq('UFA')
+  end
 
-    contract_detail = ContractDetail.find_by(contract_id: contract.id, season: '2023-2024')
-    expect(contract_detail).not_to be_nil
-    expect(contract_detail.cap_hit).to eq(6750000.to_d)
+  context" default" do
+
+    it 'imports contract data from a JSON file' do
+      # Create a team for testing
+      team = Team.create!(name: "Anaheim Ducks", code: "ANA", gm: nil, coach: nil)
+
+      # Run the rake task and capture output
+      Rake::Task[task_name].invoke(json_file_path) 
+
+      # Reload team data to check for updates
+      team.reload
+      expect(team.gm).to eq('Pat Verbeek')
+      expect(team.coach).to eq('Greg Cronin')
+
+      expect(DraftPick.all.length).to eq(21)
+      expect(Player.all.length).to eq(48)
+
+      # Check players have been imported
+      player = Player.find_by(name: 'Troy Terry')
+      expect(player).not_to be_nil
+      expect(player.position).to eq('RW, LW')
+      expect(player.career_games_played).to eq(0)
+      expect(player.contracts.length).to eq(1)
+
+      player2 = Player.find_by(name: "Mason McTavish")
+      expect(player2.contracts.length).to eq(1)
+
+      # defense_player = Player.find_by(name: 'Rasmus Andersson')
+      # expect(defense_player).not_to be_nil
+      # expect(defense_player.position).to eq('RD')
+
+      # goalie = Player.find_by(name: 'Dustin Wolf')
+      # expect(goalie).not_to be_nil
+      # expect(goalie.position).to eq('G')
+
+      # expect(player.contracts.length).to eq(1)
+      # expect(goalie.contracts.length).to eq(1)
+      # Check contracts have been created
+      # contract = Contract.find_by(player_id: goalie.id)
+      # expect(contract).not_to be_nil
+      # expect(contract.expiry_status).to eq('UFA')
+
+      # contract_detail = ContractDetail.find_by(contract_id: contract.id, season: '2023-2024')
+      # expect(contract_detail).not_to be_nil
+      # expect(contract_detail.cap_hit).to eq(6750000.to_d)
+    end
   end
 end
