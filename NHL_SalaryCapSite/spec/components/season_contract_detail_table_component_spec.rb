bc1b3cf687e51d229ce8b4ee89ed135753eb8b56
Bernie Mayer <berniemayer94@gmail.com>
2024-10-01 22:09:36 -0600

    Remove failed test

--- NHL_SalaryCapSite/spec/components/season_contract_detail_table_component_spec.rb
+++ NHL_SalaryCapSite/spec/components/season_contract_detail_table_component_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe SeasonContractDetailTableComponent, type: :component do
-  it "renders something useful" do
-    render_inline(SeasonContractDetailTableComponent.new)
-    expect(rendered_content).to include("some expected output")
-  end
+
 end
