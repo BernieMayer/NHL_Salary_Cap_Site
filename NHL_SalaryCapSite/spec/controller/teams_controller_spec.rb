6244018bd6c6d05b1ad7657f0fb20c07af2ffe11
Bernie Mayer <berniemayer94@gmail.com>
2024-10-05 16:47:46 -0600

    Add specs for show index

--- NHL_SalaryCapSite/spec/controller/teams_controller_spec.rb
+++ NHL_SalaryCapSite/spec/controller/teams_controller_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe TeamsController, type: :controller do
       expect(assigns(:teams)).to eq([team])
     end
   end
+
+  describe "Show index" do
+    let!(:team) { Team.create(name: "Calgary Flames", code: "CGY")}
+    let!(:salary_cap_total) { SalaryCapTotal.create(team: team, total: 1000, year: 2024)}
+    it "assigns @table_headers" do
+      get :show, params: { code: "CGY" }
+      expect(assigns(:table_headers)).to include("Player name", "Position", "2024-25")
+    end
+  end
 end
