f062d47d61dd2d968d281300d208e888658a7eb4
Bernie Mayer <berniemayer94@gmail.com>
2024-10-14 12:50:35 -0600

    Add ordering and styling

--- NHL_SalaryCapSite/app/controllers/players_controller.rb
+++ NHL_SalaryCapSite/app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
-    @players = Player.paginate(page: params[:page], per_page: 50)
+    @players = Player.order(:name).paginate(page: params[:page], per_page: 50)
   end  
 
   def show
