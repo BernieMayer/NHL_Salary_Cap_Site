c2c879bbdf9a151cf8485bcd52229c9f788b97a4
Bernie Mayer <berniemayer94@gmail.com>
2024-10-15 15:20:51 -0600

    Add a test for the link to show up

--- NHL_SalaryCapSite/spec/components/salary_table_component_spec.rb
+++ NHL_SalaryCapSite/spec/components/salary_table_component_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SalaryTableComponent, type: :component do
-  it 'renders something useful' do
-    seasons = (2024..2030).to_a # Example seasons data
-    player_data = [Player.create(name: "John Doe", position: "C", slug: "john-doe")]
+  include Rails.application.routes.url_helpers
 
-    # Initialize the component with the required keyword arguments
+  let!(:player) { Player.create(name: "John Doe", position: "C", slug: "john-doe")}
+  let!(:player_data ) { [player]}
+  let!(:seasons) { (2024..2030).to_a }
+
+  it 'renders something useful' do
+   
     rendered_component = render_inline(SalaryTableComponent.new(seasons: seasons, player_data: player_data))
 
     expect(rendered_component.to_html).to include('John Doe') # Check that it renders something useful
   end
+
+  it 'renders a link for the player using LinkComponent' do
+    rendered_component = render_inline(SalaryTableComponent.new(seasons: seasons, player_data: player_data))
+
+
+    expect(page).to have_selector("a")
+  end
 end
+
