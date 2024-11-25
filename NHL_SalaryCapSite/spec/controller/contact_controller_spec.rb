f027cfddbd8301add000286d40ce691659e2d3ef
Bernie Mayer <berniemayer94@gmail.com>
2024-11-25 13:32:02 -0700

    Add specs for ContractController

--- NHL_SalaryCapSite/spec/controller/contact_controller_spec.rb
+++ NHL_SalaryCapSite/spec/controller/contact_controller_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe TeamsController, type: :controller do
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe ContactController, type: :controller do
+  describe "GET #contact" do
+    it "returns a successful response" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the contact template" do
+      get :contact
+      expect(response).to render_template(:contact)
+    end
+  end
 end
