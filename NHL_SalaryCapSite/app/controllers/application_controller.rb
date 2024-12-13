325bf4ba87027e181088127ff61db0bcc265aa6d
Bernie Mayer <berniemayer94@gmail.com>
2024-12-13 16:00:18 -0700

    Add SEO optimization for the home page

--- NHL_SalaryCapSite/app/controllers/application_controller.rb
+++ NHL_SalaryCapSite/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_default_meta_tags
+
+  private
+
+  def set_default_meta_tags
+    set_meta_tags( 
+                  description: "Welcome to Cap Smarter, a website for NHL hockey salary cap data!",
+                  keywords: "rails, ruby, hockey, nhl, cap smarter",
+                  og: {
+                    title: "Cap Smarter",
+                    description: "Welcome to Cap Smarter, a website for NHL hockey salary cap data!",
+                    type: "website",
+                    url: request.url
+                  })
+  end
 end
